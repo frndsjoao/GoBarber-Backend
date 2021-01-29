0ed1b16f46d40b6fcd83142e2741cdd3851b5260
João Pedro Assunção <jfernandees002@gmail.com>
2021-01-29 15:24:12 -0300

    GoBarber backend - Node.js v0.2

---
+++ src/modules/appointments/services/ListProvidersService.spec.ts
@@ -0,0 +1,39 @@
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import ListProvidersService from './ListProvidersService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let listProviders: ListProvidersService;
+
+describe('ListProviders', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+
+    listProviders = new ListProvidersService(fakeUsersRepository);
+  });
+
+  it('should be able to list the providers', async () => {
+    const user1 = await fakeUsersRepository.create({
+      name: 'John Doe',
+      password: '123456',
+      email: 'johndoe@example.com',
+    });
+
+    const user2 = await fakeUsersRepository.create({
+      name: 'John Tre',
+      password: '123456',
+      email: 'johntre@example.com',
+    });
+
+    const loggedUser = await fakeUsersRepository.create({
+      name: 'John Qua',
+      password: '123456',
+      email: 'johnqua@example.com',
+    });
+
+    const providers = await listProviders.execute({
+      user_id: loggedUser.id,
+    });
+
+    expect(providers).toEqual([user1, user2]);
+  });
+});
